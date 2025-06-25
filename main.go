33cbea4d3710e27a85a1f0b23fca39442f2c3311
Diego Araujo <diegoaraujolive@hotmail.com>
2025-06-25 15:58:36 -0400

    generics

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-func main() {  
-   
+import "fmt"
+
+type Num interface {
+    int | int8 | int16 | int32 | int64 | float32 | float64
 }
 
-func Sum(a, b int) int {
-    return a + b
-}   
+func Add[T comparable, V Num](m map[T]V) V {
+    var sum V 
+    for _, v := range m {
+        sum += v
+    }
+    return sum
+}
+
+func main() {  
+    ints := map[string]int{
+        "a": 1,
+        "b": 2,
+        "c": 3,
+}
+
+ floats := map[string]float64{
+        "x": 1.1,
+}
+
+result := Add(ints)
+    fmt.Println("Sum of ints:", result) 
+
+    resultFloat := Add(floats)
+    fmt.Println("Sum of floats:", resultFloat)
+    
+}
